eaa0b123b40bbdb5b90fbfef6e59655969ad29ac
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-16 17:52:38 +0600

    spring boot annotation used

--- spring-boot-scratch/src/main/java/com/roze/Main.java
+++ spring-boot-scratch/src/main/java/com/roze/Main.java
@@ -1,16 +1,27 @@
 package com.roze;
 
+import com.roze.utility.College;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Main.class);
-        springApplication.run();
+        ConfigurableApplicationContext context = springApplication.run();
+        College college = context.getBean(College.class);
+        System.out.println("************");
+        System.out.println(college);
+        college.printCollegeName();
     }
 
+    @Bean
+    public College getCollegeObject() {
+        return new College();
+    }
 
 }
---
+++ spring-boot-scratch/src/main/java/com/roze/utility/College.java
@@ -0,0 +1,10 @@
+package com.roze.utility;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class College {
+    public void printCollegeName() {
+        System.out.println("IUBAT");
+    }
+}
