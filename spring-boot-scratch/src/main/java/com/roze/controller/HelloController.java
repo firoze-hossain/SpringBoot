10b7c77738c01fcfc263eaf04d89ab791768fa37
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-19 21:43:32 +0600

    restful api for spring and spring boot using jackson databind

--- spring-boot-scratch/src/main/java/com/roze/controller/HelloController.java
+++ spring-boot-scratch/src/main/java/com/roze/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.roze.controller;
 
+import com.roze.entity.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot";
+    public HelloWorld hello() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setWord1("Hello");
+        helloWorld.setWord2("Spring Boot.");
+        return helloWorld;
+
     }
 }
--- spring-hello-world-app/src/main/java/com/roze/config/HelloWorldConfig.java
+++ spring-hello-world-app/src/main/java/com/roze/config/HelloWorldConfig.java
@@ -2,7 +2,9 @@ package com.roze.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan("com.roze.controller")
 public class HelloWorldConfig {
