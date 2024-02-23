663b9c50fe4d5f6215ff924d8541ea073c843eac
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-23 22:51:49 +0600

    spring data jpa project initialized

---
+++ spring-data-jpa-demo/src/main/java/com/roze/App.java
@@ -0,0 +1,12 @@
+package com.roze;
+
+import com.roze.entity.Student;
+
+public class App {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setStudentName("Firoze");
+        student.setAddress("Dhaka");
+        student.setMobile(0171421620);
+    }
+}
