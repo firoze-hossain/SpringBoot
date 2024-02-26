bc865883f28925ab6ef3938cacdbdb2290994f13
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-26 17:49:59 +0600

    spring security dependency added

---
+++ spring-security-app/src/main/java/com/roze/config/AppSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.roze.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class AppSecurityConfig {
+}
---
+++ spring-security-app/src/main/java/com/roze/config/AppSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.roze.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//delegating filter proxy
+public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
