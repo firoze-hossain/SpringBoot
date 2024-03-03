a196987ad2ad82e4fb7615a28d4835f1d75a340b
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-03 10:17:36 +0600

    pagination with different operations

--- spring-boot-data-jpa-entity-mapping/src/test/java/com/roze/repository/CourseRepositoryTest.java
+++ spring-boot-data-jpa-entity-mapping/src/test/java/com/roze/repository/CourseRepositoryTest.java
@@ -5,6 +5,8 @@ import com.roze.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -33,6 +35,27 @@ class CourseRepositoryTest {
                 .teacher(teacher)
                 .build();
         courseRepository.save(course);
+    }
+
+    @Test
+    public void printWithPagination() {
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+        List<Course> coursesWithThreeRecords = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+        List<Course> coursesWithTwoRecords = courseRepository.findAll(secondPageWithTwoRecords).getContent();
+        long totalPagesWithThreeRecords = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+        long totalElements = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+        System.out.println("Courses with Three records: " + coursesWithThreeRecords);
+        System.out.println("Total Elements: " + totalElements);
+        System.out.println("Total Page with Three Records: " + totalPagesWithThreeRecords);
+        System.out.println("*************************");
+        long totalPagesWithTwoRecordsWithSecondPage = courseRepository.findAll(secondPageWithTwoRecords).getTotalPages();
+        totalElements = courseRepository.findAll(secondPageWithTwoRecords).getTotalElements();
+
+        System.out.println("Courses with two records: " + coursesWithTwoRecords);
+        System.out.println("Total Elements: " + totalElements);
+        System.out.println("Total Pages with two Records from second Page: " + totalPagesWithTwoRecordsWithSecondPage);
+
     }
 
 }
