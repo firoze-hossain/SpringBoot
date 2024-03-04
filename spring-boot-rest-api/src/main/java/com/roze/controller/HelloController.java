2d294a18540586c643225555a2f28784be6eca1c
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-04 14:32:10 +0600

    spring boot devtools added and configure with intellij

--- spring-boot-rest-api/src/main/java/com/roze/controller/HelloController.java
+++ spring-boot-rest-api/src/main/java/com/roze/controller/HelloController.java
@@ -1,13 +1,12 @@
 package com.roze.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String hello() {
-        return "Welcome to rest api";
+        return "Welcome to rest api....";
     }
 }
