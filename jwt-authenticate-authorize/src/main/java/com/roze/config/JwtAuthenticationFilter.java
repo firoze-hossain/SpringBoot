f3666def6ee410fc36020737878bd1faa4df0705
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-06 17:08:31 +0600

    JwtAuthenticationFilter class created with extending OncePerRequestFilter

---
+++ jwt-authenticate-authorize/src/main/java/com/roze/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.roze.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+}
