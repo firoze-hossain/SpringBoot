e1bac28c127a806a66a5c6333a9a44683a971f28
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-07 18:00:34 +0600

    create custom config in application.yml file use this with @Value annotation

--- spring-boot-rest-api/src/main/java/com/roze/controller/HelloController.java
+++ spring-boot-rest-api/src/main/java/com/roze/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.roze.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String message;
+
     @GetMapping("/")
     public String hello() {
-        return "Welcome to rest api....";
+        return message;
     }
 }
