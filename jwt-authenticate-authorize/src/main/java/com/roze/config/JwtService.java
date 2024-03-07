353eaf4efdeadacb54e23f56cd3da11d1e010d10
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-07 14:39:59 +0600

    implemented for extract all claims and set up for sign in key method and extract a single claim

--- jwt-authenticate-authorize/src/main/java/com/roze/config/JwtService.java
+++ jwt-authenticate-authorize/src/main/java/com/roze/config/JwtService.java
@@ -1,10 +1,38 @@
 package com.roze.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "0ed3e5666e34e213e129671e441fa9f4454ac92ef48740e5811390734f2f6f9a";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSingInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
