a0a20e288a063e9d7904241ece2570ec9ce85dbe
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-14 00:45:27 +0600

    create admin service

--- Ecommerce/Ecommerce-Frontend/src/app/admin/admin.module.ts
+++ Ecommerce/Ecommerce-Frontend/src/app/admin/admin.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {DemoAngularMaterailModule} from "../DemoAngularMaterailModule";
 
 
 @NgModule({
@@ -13,7 +16,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterailModule
   ]
 })
 export class AdminModule { }
