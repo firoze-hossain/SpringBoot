31e56764e84c7581802226bff02a101fb1eccd98
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-17 01:10:46 +0600

    showing all products in dashboard

--- Ecommerce/Ecommerce-Frontend/src/app/admin/service/admin.service.ts
+++ Ecommerce/Ecommerce-Frontend/src/app/admin/service/admin.service.ts
@@ -20,7 +20,9 @@ export class AdminService {
   getAllCategories(): Observable<any> {
     return this.http.get(BASIC_URL + 'api/admin/categories', {headers: this.createAuthorizationHeader()});
   }
-
+  getAllProducts(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/admin/products', {headers: this.createAuthorizationHeader()});
+  }
   addProduct(productDto: any): Observable<any> {
     return this.http.post(BASIC_URL + 'api/admin/product', productDto, {headers: this.createAuthorizationHeader()});
   }
