acffeffe9fa124a1b987a8edc96b73563a1cafe4
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-04 11:38:50 +0600

    spring boot with latest version project init

---
+++ SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/SpringBootRecapFinalApplication.java
@@ -0,0 +1,13 @@
+package com.roze.SpringBootRecapFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecapFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecapFinalApplication.class, args);
+	}
+
+}
