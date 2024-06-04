29bb71d7201900ec42f6ea73412a0a043be12d4e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-04 12:56:09 +0600

    table generator

--- JpaRecapFinal/src/main/java/com/roze/entity/Author.java
+++ JpaRecapFinal/src/main/java/com/roze/entity/Author.java
@@ -9,10 +9,17 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence")
-    @SequenceGenerator(name = "author_sequence",
-            sequenceName = "author_sequence",
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+//            generator = "author_sequence")
+//    @SequenceGenerator(name = "author_sequence",
+//            sequenceName = "author_sequence",
+//            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "author_id_gen")
+    @TableGenerator(name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
             allocationSize = 1)
     private Integer id;//we use type as Integer instead of int because by default int=0 and Integer=null
     private String firstName;
