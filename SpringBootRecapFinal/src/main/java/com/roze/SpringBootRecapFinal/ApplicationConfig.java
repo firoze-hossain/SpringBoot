df0b7072c4cdf93e51989f7513912deb1e2be385
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-07 15:26:46 +0600

    primary annotation and field injection with qualifier

--- SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/ApplicationConfig.java
+++ SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/ApplicationConfig.java
@@ -3,20 +3,27 @@ package com.roze.SpringBootRecapFinal;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
     //    @Bean
     // @Bean(name = "firoze")
     @Bean()
-    @Qualifier("bean1")
+   // @Qualifier("bean1")
     public FirstClass firstBean() {
         return new FirstClass("Welcome in Java");
     }
 
     @Bean
-    @Qualifier("bean2")
+   // @Qualifier("bean2")
     public FirstClass secondBean() {
         return new FirstClass("Welcome in Spring boot");
+    }
+    @Bean
+    //@Qualifier("bean2")
+    @Primary
+    public FirstClass thirdBean() {
+        return new FirstClass("Welcome in Third bean");
     }
 }
--- SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/MyFirstService.java
+++ SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/MyFirstService.java
@@ -1,17 +1,23 @@
 package com.roze.SpringBootRecapFinal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
-    private FirstClass firstClass;
+    @Autowired
+    @Qualifier("secondBean")
+    private  FirstClass firstClass;
 
-    public MyFirstService(
-           @Qualifier("bean2") FirstClass firstClass
-    ) {
-        this.firstClass = firstClass;
-    }
+//    public MyFirstService(
+//          // @Qualifier("bean2")
+//          FirstClass firstClass
+//    ) {
+//        this.firstClass = firstClass;
+//    }
+
+
 
     public String helloSpring() {
         return "Hello From dependency Injection: " + firstClass.hello();
