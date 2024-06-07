1065a54db618dc7274c6885510f38696788de539
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-07 14:55:40 +0600

    introduced with qualifier

--- SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/ApplicationConfig.java
+++ SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/ApplicationConfig.java
@@ -1,13 +1,22 @@
 package com.roze.SpringBootRecapFinal;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-//    @Bean
-    @Bean(name = "firoze")
-    public FirstClass firstClass() {
+    //    @Bean
+    // @Bean(name = "firoze")
+    @Bean()
+    @Qualifier("bean1")
+    public FirstClass firstBean() {
         return new FirstClass("Welcome in Java");
+    }
+
+    @Bean
+    @Qualifier("bean2")
+    public FirstClass secondBean() {
+        return new FirstClass("Welcome in Spring boot");
     }
 }
--- SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/MyFirstService.java
+++ SpringBootRecapFinal/src/main/java/com/roze/SpringBootRecapFinal/MyFirstService.java
@@ -1,12 +1,15 @@
 package com.roze.SpringBootRecapFinal;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
     private FirstClass firstClass;
 
-    public MyFirstService(FirstClass firstClass) {
+    public MyFirstService(
+           @Qualifier("bean2") FirstClass firstClass
+    ) {
         this.firstClass = firstClass;
     }
 
