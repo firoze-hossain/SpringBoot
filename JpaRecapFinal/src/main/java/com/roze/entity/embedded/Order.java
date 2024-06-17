6e7e0df705bf8fd48d73884a807c852bcbaf0255
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-17 21:26:11 +0600

    customize table name or order

--- JpaRecapFinal/src/main/java/com/roze/entity/embedded/Order.java
+++ JpaRecapFinal/src/main/java/com/roze/entity/embedded/Order.java
@@ -2,6 +2,7 @@ package com.roze.entity.embedded;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "orders")
 public class Order {
     @EmbeddedId
     private OrderId orderId;
