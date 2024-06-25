ef0be7c30ac3deb346392780b6bc83b038a98986
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-25 23:28:08 +0600

    test of converting student dto to student object

--- SpringBootRecapFinal/src/test/java/com/roze/SpringBootRecapFinal/student/StudentMapperTest.java
+++ SpringBootRecapFinal/src/test/java/com/roze/SpringBootRecapFinal/student/StudentMapperTest.java
@@ -5,33 +5,56 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentMapperTest {
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("Before all method");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("after all method");
-    }
+    //    @BeforeAll
+//    static void beforeAll() {
+//        System.out.println("Before all method");
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//        System.out.println("after all method");
+//    }
+//
+//    @BeforeEach
+//    void setUp() {
+//        System.out.println("Before each method");
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        System.out.println("After each method");
+//    }
+//
+//    @Test
+//    public void testMethod1() {
+//        System.out.println("Test method one");
+//    }
+//
+//    @Test
+//    public void testMethod2() {
+//        System.out.println("Test method two");
+//    }
+    private StudentMapper studentMapper;
 
     @BeforeEach
     void setUp() {
-        System.out.println("Before each method");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.out.println("After each method");
+        studentMapper = new StudentMapper();
     }
 
     @Test
-    public void testMethod1() {
-        System.out.println("Test method one");
-    }
+    public void shouldMapStudentDtoToStudent() {
+        StudentRequestDto studentRequestDto = new StudentRequestDto(
+                "Firoze",
+                "Hossain",
+                "firoze@gmail.com",
+                1
+        );
+        Student student = studentMapper.toStudent(studentRequestDto);
+        assertEquals(studentRequestDto.firstName(), student.getFirstName());
+        assertEquals(studentRequestDto.lastName(), student.getLastName());
+        assertEquals(studentRequestDto.email(), student.getEmail());
+        assertNotNull(student.getSchool());
+        assertEquals(studentRequestDto.schoolId(), student.getSchool().getId());
 
-    @Test
-    public void testMethod2() {
-        System.out.println("Test method two");
     }
 }
