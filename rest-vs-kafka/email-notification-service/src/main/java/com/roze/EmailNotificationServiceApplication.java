feb984b7e4c2330114d518d2effd053536bbab92
firoze-hossain <firoze.hossain@outlook.com>
2025-09-15 15:39:58 +0600

    convert rest call to event driven call

--- rest-vs-kafka/email-notification-service/src/main/java/com/roze/EmailNotificationServiceApplication.java
+++ rest-vs-kafka/email-notification-service/src/main/java/com/roze/EmailNotificationServiceApplication.java
@@ -2,12 +2,18 @@ package com.roze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class EmailNotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmailNotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "new-user-events", groupId = "email-service")
+    public void senEmail(String username) {
+        System.out.println("EmailNotificationService-> sending mail to: " + username);
     }
 
 }
--- rest-vs-kafka/user-service/src/main/java/com/roze/controller/UserController.java
+++ rest-vs-kafka/user-service/src/main/java/com/roze/controller/UserController.java
@@ -1,24 +1,26 @@
 package com.roze.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    RestTemplate restTemplate = new RestTemplate();
+    //RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping("/{name}")
     public String create(@PathVariable String name) {
-        Map<String, String> userDetails = new HashMap<>();
-        userDetails.put("username", name);
-        String response = restTemplate.getForObject("http://localhost:8081/emails/sendemail/{username}", String.class, userDetails);
-        System.out.println("Email Sent to: " + response);
+//        Map<String, String> userDetails = new HashMap<>();
+//        userDetails.put("username", name);
+//        String response = restTemplate.getForObject("http://localhost:8081/emails/sendemail/{username}", String.class, userDetails);
+//        System.out.println("Email Sent to: " + response);
+        kafkaTemplate.send("new-user-events", name);
         return "User created: " + name;
     }
 }
