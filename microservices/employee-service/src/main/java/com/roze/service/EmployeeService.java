a264c3ce6787f4d329d2fd4d51514f53a46549a9
firoze-hossain <firoze.hossain@outlook.com>
2025-09-17 21:29:16 +0600

    address api call in employee using restTemplate

--- microservices/employee-service/src/main/java/com/roze/service/EmployeeService.java
+++ microservices/employee-service/src/main/java/com/roze/service/EmployeeService.java
@@ -2,10 +2,12 @@ package com.roze.service;
 
 import com.roze.entity.Employee;
 import com.roze.repository.EmployeeRepository;
+import com.roze.response.AddressResponse;
 import com.roze.response.EmployeeResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeService {
@@ -13,15 +15,15 @@ public class EmployeeService {
     private EmployeeRepository employeeRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public EmployeeResponse getEmployeeById(int id) {
+
         Employee employee = employeeRepository.findById(id).get();
-//        EmployeeResponse response = new EmployeeResponse();
-//        response.setId(employee.getId());
-//        response.setEmail(employee.getEmail());
-//        response.setBloodGroup(employee.getBloodGroup());
-//        response.setName(employee.getName());
         EmployeeResponse response = modelMapper.map(employee, EmployeeResponse.class);
+        AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8080/address-app/api/address/{id}", AddressResponse.class, id);
+        response.setAddressResponse(addressResponse);
         return response;
     }
 }
