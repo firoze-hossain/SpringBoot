787e371112e223b91f8a408c11408b53d5c4387d
firoze-hossain <firoze.hossain@outlook.com>
2025-09-25 16:17:25 +0600

    first reactive api done

--- spring-reactive-with-webflux/reactive-with-webflux-01/src/main/java/com/roze/controller/HelloController.java
+++ spring-reactive-with-webflux/reactive-with-webflux-01/src/main/java/com/roze/controller/HelloController.java
@@ -3,21 +3,24 @@ package com.roze.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
 
 @Controller
 public class HelloController {
     @ResponseBody
     @GetMapping("/hello")
-    public List<String> hello() throws InterruptedException {
-        List<String> returnList = new ArrayList<>();
+//    public List<String> hello() throws InterruptedException {
+    public Flux<String> hello() throws InterruptedException {
+        //  List<String> returnList = new ArrayList<>();
         List<String> stringList = List.of("Firoze", "Millat", "Mydul", "Imtiaze");
-        for (String str : stringList) {
-            returnList.add(str);
-            Thread.sleep(1000);
-        }
-        return returnList;
+//        for (String str : stringList) {
+//            returnList.add(str);
+//            Thread.sleep(1000);
+//        }
+        Flux<String> publisher = Flux.fromIterable(stringList).delayElements(Duration.ofSeconds(2)).log();
+        return publisher;
     }
 }
