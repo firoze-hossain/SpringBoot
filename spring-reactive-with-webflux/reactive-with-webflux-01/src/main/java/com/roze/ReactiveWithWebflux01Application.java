1c289d1f48bca0ed80b9e5217ee3a513d12fe31e
firoze-hossain <firoze.hossain@outlook.com>
2025-09-25 13:23:22 +0600

    new reactive project setup

---
+++ spring-reactive-with-webflux/reactive-with-webflux-01/src/main/java/com/roze/ReactiveWithWebflux01Application.java
@@ -0,0 +1,13 @@
+package com.roze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWithWebflux01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWithWebflux01Application.class, args);
+    }
+
+}
