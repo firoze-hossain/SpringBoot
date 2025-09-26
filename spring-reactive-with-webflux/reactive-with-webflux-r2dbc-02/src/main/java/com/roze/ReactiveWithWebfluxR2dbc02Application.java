ed520a7b9ee4062f7b72666e5a55a93afc2b0449
firoze-hossain <firoze.hossain@outlook.com>
2025-09-26 06:39:02 +0600

    create test endpoint with flux

---
+++ spring-reactive-with-webflux/reactive-with-webflux-r2dbc-02/src/main/java/com/roze/ReactiveWithWebfluxR2dbc02Application.java
@@ -0,0 +1,13 @@
+package com.roze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWithWebfluxR2dbc02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWithWebfluxR2dbc02Application.class, args);
+    }
+
+}
